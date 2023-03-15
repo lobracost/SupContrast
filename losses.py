9f05cb47f844976c96ace002b75835794f508af1
lobracost <pa.koromilas@gmail.com>
2023-03-15 23:47:18 +0200

    minor

--- losses.py
+++ losses.py
@@ -94,7 +94,8 @@ class SupConLoss(nn.Module):
         mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
 
         # loss
-        loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
+        #loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
+        loss = - mean_log_prob_pos
         loss = loss.view(anchor_count, batch_size).mean()
 
         return loss
@@ -187,7 +188,8 @@ class SupConLossOCC(nn.Module):
         mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
 
         # loss
-        loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
+        #loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
+        loss = - mean_log_prob_pos
         loss = loss.view(anchor_count, batch_size).mean()
         # TODO: sum or mean??
         return loss
